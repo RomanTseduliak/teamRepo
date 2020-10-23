1c03f034c23b996f0bf96a40873faeb367babc0b
Roman <roman.kushka10@gmail.com>
2020-10-23 13:16:58 +0300

    layer1

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.slider').slick({
-        nextArrow: '<button class="slick-arrow slick-next"><img src="img/next-button.png" alt="next arrow"></button>',
-        prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev-button.png" alt="prev arrow"></button>'
-    });
-});
-
-
